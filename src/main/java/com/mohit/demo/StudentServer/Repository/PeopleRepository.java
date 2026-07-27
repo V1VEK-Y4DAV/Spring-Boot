483947461b62f667fad9f8de2fa10a858dd4eb19
V1VEK-Y4DAV <vivekkuyadav21@gmail.com>
2026-07-27 12:26:58 +0530

    added spring security

---
+++ src/main/java/com/mohit/demo/StudentServer/Repository/PeopleRepository.java
@@ -0,0 +1,10 @@
+package com.mohit.demo.StudentServer.Repository;
+
+import com.mohit.demo.StudentServer.Entity.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PeopleRepository extends JpaRepository<People,Integer> {
+    Optional<People> findByUsername(String username);
+}
---
+++ src/main/java/com/mohit/demo/StudentServer/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mohit.demo.StudentServer.Repository;
+
+import com.mohit.demo.StudentServer.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+}
